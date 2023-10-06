various fixes to allow gradients between recurrent connections

run_training.py
@@ -85,11 +85,16 @@ frs_all = np.asarray(frs_all)
 place_information = {'place ids': [0], 'place fracs': [0.80]}
 
 diagram = WiringDiagram(os.path.join(params_path, 'circuitparams.yaml'), place_information)
-diagram.generate_internal_connectivity()
-
 
 place_ids = diagram.place_information[0]['place']
 cue_ids = diagram.place_information[0]['not place']
+
+internal_kwargs = {}
+internal_kwargs['place information'] = diagram.place_information
+internal_kwargs['cue information'] = diagram.place_information
+
+diagram.generate_internal_connectivity(**internal_kwargs)
+
 
 external_kwargs = {}
 external_kwargs['place information'] = diagram.place_information
@@ -203,7 +208,6 @@ tic = time.time()
 h.dt = 0.025
 h.celsius = 37.
 h.tstop =  time_for_single_lap * nlaps + 500
-h.tstop = 1000.
 
 if pc.id() == 0:
     print(f'starting simulation for {nlaps} lap(s) until {h.tstop} ms..')

utils/SetupConnections.py
@@ -182,7 +182,7 @@ class Arena(object):
             
     def generate_cue_firing_rates(self, population, percent_cue, seed=1e9):
             rnd = np.random.RandomState(seed=int(seed))
-            noise_fr    = self.params['Spatial'][population]['noise']['min rate']
+            noise_fr    = self.params['Spatial'][population]['noise']['mean rate']
             ncells      = self.params['Spatial'][population]['ncells']
             
             ncue_cells = int(ncells*percent_cue)
@@ -218,8 +218,11 @@ class Arena(object):
         population_info = self.cell_information[population]
         ncells = population_info['ncells']
         gids   = range(int(self.pc.id()), ncells, int(self.pc.nhost()))
+
         
-        nfr      = self.params['Spatial'][population]['noise']['min rate']
+        nfr      = np.clip(self.arena_rnd.normal(self.params['Spatial'][population]['noise']['mean rate'],
+                                                 scale=self.params['Spatial'][population]['noise']['scale']),
+                           0.0, None)
         noise_fr = [nfr for _ in range(len(self.arena_map))]
         firing_rates = {}
         for gid in gids:
@@ -406,7 +409,16 @@ class WiringDiagram(object):
             ctype_offset += ncells[i]
                 
         
-    def generate_internal_connectivity(self):
+    def generate_internal_connectivity(self, **kwargs):
+
+        PYR_cue_weight_mean = 1.0
+        PYR_cue_weight_scale = 0.025
+        PYR_place_weight_mean = 0.6
+        PYR_place_weight_scale = 0.025
+
+        place_information  = kwargs['place information']
+        cue_information = kwargs['cue information']
+        
         self.internal_adj_matrices = {}
         self.internal_ws = {}
         for popA in self.pops:
@@ -418,16 +430,37 @@ class WiringDiagram(object):
                 popA_pos = self._get_soma_coordinates(self.wiring_information[popA]['cell info'])
                 popB_pos = self._get_soma_coordinates(self.wiring_information[popB]['cell info'])
                 nA, nB = self.wiring_information[popA]['ncells'], self.wiring_information[popB]['ncells']
+
+                weight_scale_func = None
+                if popA_id == 0 and popB_id == 0:
+                    place_gids = place_information[popB_id]['place']
+                    place_gid_set = set(place_gids)
+                
+                    cue_gids = cue_information[popB_id]['not place']
+                    cue_gid_set = set(cue_gids)
+
+                    PYR_cue_weights = np.clip(self.internal_con_rnd.normal(PYR_cue_weight_mean,
+                                                                           PYR_cue_weight_scale,
+                                                                           nB), 1e-4, None)
+                    PYR_place_weights = np.clip(self.internal_con_rnd.normal(PYR_place_weight_mean,
+                                                                             PYR_place_weight_scale,
+                                                                             nB), 1e-4, None)
+                    weight_scale_func = None
+                    weight_scale_func = lambda dst_gid, src_gid: PYR_cue_weights[dst_gid] if src_gid in cue_gid_set else PYR_place_weights[dst_gid]
+
                 try:
                     convergence = self.params['internal connectivity'][popA_id][popB_id]['probability']
                     same_pop = False
+
                     if popA == popB: same_pop=True
                     if popA_id == 0 and popB_id == 0:
                         alpha = 0.0125
                         am, ws = self.create_adjacency_matrix(popA_pos, popB_pos, nA, nB, convergence, self.internal_con_rnd, 
-                                                          inv_func=('exp', alpha), same_pop=same_pop, src_id=popA_id)
+                                                              inv_func=('exp', alpha), same_pop=same_pop, src_id=popA_id,
+                                                              weight_scale_func=weight_scale_func)
                     else:
-                        am, ws = self.create_adjacency_matrix(None, None, nA, nB, convergence, self.internal_con_rnd, same_pop=same_pop)   
+                        am, ws = self.create_adjacency_matrix(None, None, nA, nB, convergence, self.internal_con_rnd, same_pop=same_pop,
+                                                              weight_scale_func=weight_scale_func)
                     self.internal_adj_matrices[popA_id][popB_id] = am
                     self.internal_ws[popA_id][popB_id] = ws
                 except:
@@ -446,12 +479,17 @@ class WiringDiagram(object):
         external_pops = list(external_information.keys())
         external_ids  = [external_information[pop]['id'] for pop in external_pops]
 
-        LEC_cue_weight_mean = 0.9
+        MF_cue_weight_mean = 0.5
+        MF_cue_weight_scale = 0.025
+        MF_place_weight_mean = 1.0
+        MF_place_weight_scale = 0.025
+
+        LEC_cue_weight_mean = 1.0
         LEC_cue_weight_scale = 0.025
         LEC_place_weight_mean = 0.2
         LEC_place_weight_scale = 0.025
 
-        MEC_cue_weight_mean = 0.3
+        MEC_cue_weight_mean = 0.4
         MEC_cue_weight_scale = 0.025
         MEC_place_weight_mean = 1.0
         MEC_place_weight_scale = 0.025
@@ -518,20 +556,28 @@ class WiringDiagram(object):
                       f"src_id in external_place_ids = {src_id in external_place_ids} ")
 
                 
+                MF_cue_weights = np.clip(self.external_con_rnd.normal(MF_cue_weight_mean,
+                                                                      MF_cue_weight_scale,
+                                                                      ndst), 1e-3, None)
+                MF_place_weights = np.clip(self.external_con_rnd.normal(MF_place_weight_mean,
+                                                                        MF_place_weight_scale,
+                                                                        ndst), 1e-3, None)
                 LEC_cue_weights = np.clip(self.external_con_rnd.normal(LEC_cue_weight_mean,
                                                                        LEC_cue_weight_scale,
-                                                                       ndst), 0.0, None)
+                                                                       ndst), 1e-3, None)
                 LEC_place_weights = np.clip(self.external_con_rnd.normal(LEC_place_weight_mean,
                                                                          LEC_place_weight_scale,
-                                                                         ndst), 0.0, None)
+                                                                         ndst), 1e-3, None)
                 MEC_cue_weights = np.clip(self.external_con_rnd.normal(MEC_cue_weight_mean,
                                                                        MEC_cue_weight_scale,
-                                                                       ndst), 0.0, None)
+                                                                       ndst), 1e-3, None)
                 MEC_place_weights = np.clip(self.external_con_rnd.normal(MEC_place_weight_mean,
                                                                          MEC_place_weight_scale,
-                                                                         ndst), 0.0, None)
+                                                                         ndst), 1e-3, None)
                                             
                 weight_scale_func = None
+                if dst_pop_id == 0 and src_id == 100:
+                    weight_scale_func = lambda dst_gid, src_gid: MF_cue_weights[dst_gid] if dst_gid in cue_gid_set else MF_place_weights[dst_gid]
                 if dst_pop_id == 0 and src_id == 101:
                     weight_scale_func = lambda dst_gid, src_gid: MEC_cue_weights[dst_gid] if dst_gid in cue_gid_set else MEC_place_weights[dst_gid]
                 if dst_pop_id == 0 and src_id == 102:
@@ -649,7 +695,7 @@ class WiringDiagram(object):
             for gid in presynaptic_gids:
                 adj_mat[gid,d] += 1
                 if weight_scale_func is not None:
-                    weight_scales[(gid,d)] = weight_scale_func(gid, d)
+                    weight_scales[(gid,d)] = weight_scale_func(d, gid)
                     
         return adj_mat, weight_scales
                 

utils/ca3_neuron_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 from neuron import h
 
 def _make_synapse(stype, compartment, weight, synapse_information, srcid, dstid, dst_cell, c, params):
@@ -14,7 +15,7 @@ def _make_synapse(stype, compartment, weight, synapse_information, srcid, dstid,
         syn_params['e']    = -75.0
         syn_params['tau1'] = 35.0
         syn_params['tau2'] = 100.0
-    elif stype =='STDPE2' or stype == 'STDPE2ASYM':  
+    elif stype =='STDPE2' or stype == 'STDPE2ASYM':
         if dstid == 0:
             syn_params = dst_cell.get_syn_parameters(compartment, stype)
         else:
@@ -24,13 +25,13 @@ def _make_synapse(stype, compartment, weight, synapse_information, srcid, dstid,
         pparams = None
         if stype == 'STDPE2': pparams = params['symplasticity']
         else: pparams = params['asymplasticity']
+        
         syn_params['p']  = pparams.get('potentiation', 1.0)
         syn_params['d']  = pparams.get('depression', 0.4)
         syn_params['thresh'] = pparams.get('thresh', -10.)
         syn_params['wmax']   = pparams.get('wmax_scaler', 1.0) * weight
         syn_params['dtau']   = pparams.get('dtau', 34.0)
         syn_params['ptau']   = pparams.get('ptau', 17.0)   
-
         syn_params['p'] = synapse_information.get('potentiation', syn_params['p']) / params['scale']
         syn_params['d'] = synapse_information.get('depression', syn_params['d']) / params['scale']
         syn_params['dtau']   = synapse_information.get('dtau', syn_params['dtau'])
@@ -62,6 +63,7 @@ def create_netcon(pc, srcid, dstid, src_gid, dst_cell, synapse_information, comp
     if params['scale'] > 1:
         weight /= (float(params['scale']))
     ws = netcon_params.get('weight_scale', 1.0)
+        
     weight *= float(ws)
     weight0 = netcon_params.get('weight0', None)
     weight_upd = netcon_params.get('weight_upd', None)
@@ -71,13 +73,22 @@ def create_netcon(pc, srcid, dstid, src_gid, dst_cell, synapse_information, comp
         synapse_type = [synapse_type]
     ncs = []
     for idx, stype in enumerate(synapse_type):
+        syn_params = copy.deepcopy(params)
+        if stype =='STDPE2':
+            if ('weight_scale' in netcon_params):
+                syn_params['symplasticity']['wmax_scaler'] = params['symplasticity']['wmax_scaler'] * ws
+        elif stype == 'STDPE2ASYM':
+            if ('weight_scale' in netcon_params):
+                syn_params['asymplasticity']['wmax_scaler'] = params['asymplasticity']['wmax_scaler'] * ws
+
         c = None
         if srcid not in dst_cell.synGroups[stype][compartment]: 
             c = getattr(dst_cell, compartment)
             dst_cell.synGroups[stype][compartment][srcid] = []
         syn_ = None
         if c is not None: # if synape doesn't exist, build it
-            syn_ = _make_synapse(stype, compartment, weight, synapse_information, srcid, dstid, dst_cell, c, params)
+            syn_ = _make_synapse(stype, compartment, weight, synapse_information,
+                                 srcid, dstid, dst_cell, c, syn_params)
             dst_cell.synGroups[stype][compartment][srcid].append(syn_)
         else:
             syn_ = dst_cell.synGroups[stype][compartment][srcid][0]
@@ -85,11 +96,14 @@ def create_netcon(pc, srcid, dstid, src_gid, dst_cell, synapse_information, comp
         nc = pc.gid_connect(src_gid, syn_)
         nc.delay     = 1.
         nc.weight[0] = weight
-        if nc.wcnt() > 1:
-            if (weight_upd is not None) and (weight_upd != np.nan):
+        if stype =='STDPE2' or stype == 'STDPE2ASYM':
+            nc.weight[1] = 0.
+        if (weight_upd is not None) and (not(np.isnan(weight_upd))):
+            if stype =='STDPE2' or stype == 'STDPE2ASYM':
                 nc.weight[1] = weight_upd
             else:
-                nc.weight[1] = 0.
+                nc.weight[0] = weight_upd
+
         ncs.append(nc)
         
     return ncs